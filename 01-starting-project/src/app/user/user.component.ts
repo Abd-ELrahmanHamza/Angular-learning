8bc1bf73f71f8d61da30a2712f01a5c6899fbba8
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-08-06 17:51:57 +0300

    computed properties and events

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[0];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('clicked');
+  }
+}
