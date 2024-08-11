55e2cd1906998615ac24a5ac9ab47b7ef07aa17e
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-08-12 02:04:32 +0300

    add template driven forms

---
+++ template-forms-contacts/src/app/edit-contact/edit-contact.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Contact } from '../contacts/contact.model';
+
+@Component({
+  imports: [CommonModule, FormsModule],
+  standalone: true,
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.css'],
+})
+export class EditContactComponent implements OnInit {
+  contact: Contact = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: null,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  };
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const contactId = this.route.snapshot.params['id'];
+    if (!contactId) return;
+  }
+
+  saveContact() {
+    console.log('Save contact', this.contact);
+  }
+}
